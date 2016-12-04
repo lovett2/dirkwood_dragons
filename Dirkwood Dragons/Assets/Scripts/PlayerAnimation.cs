15693f0630fd57e06128efcad684b71f75d09851
Sir-HC <lynchzor@gmail.com>
2016-12-04 12:38:19 -0800

    Re-aranging player movement seperate from animation

---
+++ Dirkwood Dragons/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool jump = false;
+
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		float h = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("speed", Mathf.Abs (h));
+
+		if (h > 0 && !facingRight)
+			Flip ();
+		else if (h < 0 && facingRight)
+			Flip();
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Dirkwood Dragons/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	private Rigidbody2D rb2d;
+
+	private int jumpTimer = 0;
+	private int maxJumpTime = 50;
+	private int jumpLaunch = 0;
+	private int maxJumpLaunch = 1;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundLength = 0.2f;
+	public LayerMask whatIsGround;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	/*
+	void OnCollisionEnter(Collider col)
+	{
+		Debug.Log ("Collision Detected!");
+		if (col.gameObject.name == "ground") {
+			jumpTimer = 0;
+			jumpLaunch = 0;
+		}
+	}
+	*/
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapBox(groundCheck.position, new Vector2(groundLength, groundLength), whatIsGround);
+
+		float h = Input.GetAxis ("Horizontal");
+		rb2d.velocity = new Vector2 (h, 0);
+
+		if (Input.GetKeyUp(KeyCode.Space))
+			jumpLaunch++;
+
+		if (Input.GetKey (KeyCode.Space) && jumpTimer < maxJumpTime && jumpLaunch < maxJumpLaunch) {
+			rb2d.velocity = new Vector2 (h, 1);// (Vector2.up * 4);
+			jumpTimer++;
+			Debug.Log (jumpTimer);
+		}
+	}
+}
